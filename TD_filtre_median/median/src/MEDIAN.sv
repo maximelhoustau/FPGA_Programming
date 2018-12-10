5bb900010f7822bf0793e5866cc60877da0fadf9
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2018-12-10 09:37:47 +0100

    Travail sur la résolution d'erreur Illegal reference to net 'DSO' et 'BYP'

--- TD_filtre_median/median/src/MEDIAN.sv
+++ TD_filtre_median/median/src/MEDIAN.sv
@@ -3,13 +3,25 @@ module MEDIAN #(parameter W = 8) (input [W-1:0] DI, input DSI, input nRST, input
 MED #(.W(8)) med (.DI(DI), .DSI(DSI), .BYP(BYP), .CLK(CLK), .DO(DO));
 
 enum logic [2:0] {S0, S1, S2, S3, S4} state, n_state;
-logic [3:0] tmp = 0;
+int tmp;
 
+//Processus de gestion du compteur tmp
 always_ff @(posedge CLK or negedge nRST) 
-	if (nRST)
-		state <= S0 ; 
-	
+	if (!nRST)
+		tmp <= 0;
 	else
+	begin
+		if (state == S0  & n_state == S4) 
+			tmp <= 0;
+		else 
+			tmp <= tmp + 1;
+	end
+
+//Processus de gestion de changement d'éat de l'automate à 5 états
+always_ff @(posedge CLK or negedge nRST) 
+	if (!nRST)
+		state <= S0 ;	
+	else begin
 		case (state)	
 	       	S0 : if (tmp == 9)
 			state <= S1; 
@@ -23,13 +35,13 @@ always_ff @(posedge CLK or negedge nRST)
 			state <= S0;	
 		end
 		endcase
+	end
 
 always_comb
 begin
-	tmp++;
 	if (state == S0) begin
 		DSO <= 0;
-		while(tmp < 9 ) begin
+		if(tmp < 9 ) begin
 			BYP <= 0; 
 		end
 		else begin
@@ -39,7 +51,7 @@ begin
 
 	if (state == S1) begin
 		DSO <= 0;
-		while(tmp < 17 ) begin
+		if(tmp < 17 ) begin
 			BYP <= 0;
 		end
 		else begin
@@ -49,7 +61,7 @@ begin
 
 	if (state == S2) begin
 		DSO <= 0;
-		while(tmp < 25 ) begin
+		if(tmp < 25 ) begin
 			BYP <= 0;
 		end
 		else begin
@@ -58,8 +70,8 @@ begin
 	end
 
 	if (state == S2) begin
-		DS0 <= 0;
-		while(tmp < 34 ) begin
+		DSO <= 0;
+		if(tmp < 34 ) begin
 			BYP <= 0;
 			end
 		else begin
@@ -68,8 +80,8 @@ begin
 	end
 
 	if (state == S3) begin
-		DS0 <= 0;
-		while(tmp < 42 ) begin
+		DSO <= 0;
+		if(tmp < 42 ) begin
 			BYP <= 0;
 		end
 		else begin
@@ -80,7 +92,6 @@ begin
 	if (state == S4) begin
 		BYP <= 0;
 		DSO <= 1;
-		tmp = 0;
 	end
 
 end
