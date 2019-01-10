e3afa9c3016a6721675cf88c4984b3db50c7e511
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-01-10 11:15:14 +0100

    Synthese partie 2 et modification de la constante noire dans vga (taille fixée)

--- SoCFPGA/src/vga.sv
+++ SoCFPGA/src/vga.sv
@@ -78,7 +78,7 @@ always_comb
 begin
 	pixel_X <= pixels - (HSUP-1); 
 	pixel_Y <= lignes - (VSUP); 
-	video_ifm.RGB <= (pixel_X%16 == 0 || pixel_Y%16 == 0)? {8'hff, 8'hff,8'hff}: {0,0,0};
+	video_ifm.RGB <= (pixel_X%16 == 0 || pixel_Y%16 == 0)? {8'hff, 8'hff,8'hff}: {8'h0,8'h0,8'h0};
 end
 
 endmodule
--- SoCFPGA/syn/scripts/project_list.tcl
+++ SoCFPGA/syn/scripts/project_list.tcl
@@ -1,8 +1,8 @@
 set PROJECT_DIR  $env(PROJECT_DIR)
 
 #Definition des fichiers sources vga
-$PROJECT_DIR/ips/interfaces/video_if.sv
-$PROJECT_DIR/SoCFPGA/src/vga.sv
+set_global_assignment -name SYSTEMVERILOG_FILE $PROJECT_DIR/ips/interfaces/video_if.sv
+set_global_assignment -name SYSTEMVERILOG_FILE $PROJECT_DIR/SoCFPGA/src/vga.sv
 
 # Placer ici la liste des modules complémentaires nécessaires à la synthèse
 # Tout ce qui concerne le "hw_support" est décrit dans un fichier a part
