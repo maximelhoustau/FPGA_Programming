6a7c39a1e06445077b5a5dfab0397cae92001125
Maxime Lhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-01-14 19:26:58 +0100

    Travail en cours sur la lecture de la FIFO

--- SoCFPGA/src/vga.sv
+++ SoCFPGA/src/vga.sv
@@ -34,26 +34,12 @@ logic write;
 logic wfull;
 logic walmost_full;
 
-
 //Instanciation de la FIFO asynchrone
 async_fifo #(.DATA_WIDTH(32)) fifo (.rst(wshb_ifm.rst), .rclk(wshb_ifm.clk), .read(read), .rdata(rdata), .rempty(rempty), .wclk(wshb_ifm.clk), .wdata(wdata), .write(write), .wfull(wfull), .walmost_full(walmost_full));
-
-
 
 //Clock video
 assign video_ifm.CLK = pixel_clk;
 
-//Signaux de l'interface wshb_if
-/*
-assign wshb_ifm.dat_ms = 32'hBABECAFE;
-assign wshb_ifm.adr = '0;
-assign wshb_ifm.cyc = 1'b1;
-assign wshb_ifm.sel = 4'b1111;
-assign wshb_ifm.stb = 1'b1;
-assign wshb_ifm.we = 1'b1;
-assign wshb_ifm.cti = '0;
-assign wshb_ifm.bte = '0;
-*/
 //Compteur de lignes et de colonnes
 always_ff @(posedge pixel_clk or posedge pixel_rst)
 begin
@@ -99,7 +85,7 @@ begin
 		video_ifm.BLANK = 1;
 
 end
-
+/*
 //Génération de la mire de test et calcul des coordonnées du pixel actif
 always_comb 
 begin
@@ -107,11 +93,13 @@ begin
 	pixel_Y = lignes - (VSUP); 
 	video_ifm.RGB = (pixel_X%16 == 0 || pixel_Y%16 == 0)? {8'hff, 8'hff,8'hff}: {8'h0,8'h0,8'h0};
 end
-
+*/
+//Lecture en SDRAM et ecriture dans la FIFO
 //Compteur pour coordonnées des pixels lus en mémoire
 logic[$clog2(HDISP)-1:0] X;
 logic[$clog2(VDISP)-1:0] Y;
-//Lecture en SDRAM
+
+//Signaux bus Wishbone
 assign wshb_ifm.cyc = 1'b1; //On sollicite l'esclave en permanence
 assign wshb_ifm.we = 1'b0; //Ecriture
 assign wshb_ifm.cti = 3'b10; //Transfert classique
@@ -121,7 +109,6 @@ assign wshb_ifm.adr = 4*(HDISP*Y + X);
 assign wshb_ifm.stb = 1'b1;
 
 assign wdata = wshb_ifm.dat_sm;
-
 always_ff @(posedge wshb_ifm.clk or posedge wshb_ifm.rst)
 begin
 	//Lecture en continue
@@ -150,5 +137,49 @@ begin
 	end
 end
 
+
+//La FIFO a-t-elle été vide au moins une fois avant la zone d'affichage
+logic first_read;
+logic fifo_full;
+
+always_comb
+begin
+	if(fifo_full && video_ifm.BLANK)
+		first_read = 1'b1;
+end
+
+//Ecriture dans la FIFO
+always_ff @(posedge pixel_clk)
+begin
+	if(first_read)
+	begin
+		if(video_ifm.BLANK >= 0)
+		begin
+			read <= 1'b1;
+			video_ifm.RGB <= rdata;
+		end
+		else
+			read <= 1'b0;
+	end
+	else
+		read <= 1'b0;
+	
+end
+
+//Strategie R1 pour passer wfull dans le domaine de pixel_clk
+//2 bascules dans le domaine de pixel_clk
+always_ff @(posedge pixel_clk or pixel_rst)
+begin
+	logic Q1; //Signal entre les 2 bascules
+	if(pixel_rst)
+	begin
+		Q1 <= wfull;
+		fifo_full <= wfull;
+	end
+	else begin 
+		Q1 <= wfull;
+		fifo_full <= Q1;
+	end
+end
 
 endmodule
